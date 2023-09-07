4df0ef7f48dc0ac867ee1c75876ad155f94b2ed7
MacKenzie Pugh <mkp5761@psu.edu>
2023-09-07 17:32:12 -0400

    add changes

---
+++ build/CMakeFiles/Quiz1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Quiz1.dir/main.cpp.o"
+  "CMakeFiles/Quiz1.dir/main.cpp.o.d"
+  "Quiz1"
+  "Quiz1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Quiz1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
